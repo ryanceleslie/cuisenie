3384aa08f76cf5bdc3a5d1ed3fdaa8c140821cae
rceleslie <ryan.celeslie@cbts.net>
2019-01-26 10:29:30 -0500

    Finalized migration for RecipeAggregate

--- MealPlanner/Infrastructure/Data/Migrations/20190126152422_AddedDefaultValuesToBaseEntity.Designer.cs
+++ MealPlanner/Infrastructure/Data/Migrations/20190126152422_AddedDefaultValuesToBaseEntity.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(MealPlannerContext))]
-    partial class MealPlannerContextModelSnapshot : ModelSnapshot
+    [Migration("20190126152422_AddedDefaultValuesToBaseEntity")]
+    partial class AddedDefaultValuesToBaseEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
