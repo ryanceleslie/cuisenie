ff7b1e492c347dc4ca9be8fc5638b32c12d6e442
rceleslie <ryan.celeslie@cbts.net>
2019-01-27 01:06:32 -0500

    Added basic test for repo, still WIP, and added some mocking and a provider

---
+++ MealPlanner/UnitTests/Infrastructure/Data/RepositoryTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using FluentAssertions;
+using UnitTests.Utilities;
+using Infrastructure.Data;
+
+namespace UnitTests.Infrastructure.Data
+{
+    public class RepositoryTests<T> where T : BaseEntity
+    {
+        public MockDbSet<T> _dbSet { get; set; }
+        public Mock<MealPlannerContext> _context { get; set; }
+        public IRepository<T> _repo { get; set; }
+        public IAsyncRepository<T> _asyncRepo { get; set; }
+
+        #region Add
+
+        // test for type
+        public void Test_Add_Entity_Returns_Type(T entity)
+        {
+            // Arrange
+            _context.Setup(c => c.Set<T>())
+                .Returns(_dbSet.Object);
+
+            // Act
+            var act = _repo.Add(entity);
+
+            // Assert
+            act.Should().BeOfType<T>();
+        }
+
+        // test for type async
+        public async Task Test_AddAsync_Entity_Returns_Type(T entity)
+        {
+            // Arrange
+            _context.Setup(c => c.Set<T>())
+                .Returns(_dbSet.Object);
+
+            // Act
+            var act = await _asyncRepo.AddAsync(entity);
+
+            // Assert
+            act.Should().BeOfType<T>();
+        }
+
+        // test for entity
+        public void Test_Add_Entity_Returns_Entity(T entity)
+        {
+            // Arrange
+            var mockEntity = entity;
+            _context.Setup(c => c.Set<T>())
+                .Returns(_dbSet.Object);
+
+            // Act
+            var act = _repo.Add(entity);
+
+            // Assert
+            act.Should().BeEquivalentTo(mockEntity);
+        }
+
+        // test for entity async
+        public async Task Test_AddAsync_Returns_Entity(T entity)
+        {
+            // Arrange
+            var mockEntity = entity;
+            _context.Setup(c => c.Set<T>())
+                .Returns(_dbSet.Object);
+
+            // Act
+            var act = await _asyncRepo.AddAsync(entity);
+
+            // Assert
+            act.Should().BeEquivalentTo(mockEntity);
+        }
+        #endregion
+
+        #region Count
+
+
+
+        #endregion
+
+        #region Delete
+
+
+
+        #endregion
+
+        #region GetById
+
+
+
+        #endregion
+
+        #region GetSingleBySpec
+
+
+
+        #endregion
+
+        #region List
+
+
+
+        #endregion
+
+        #region Update
+
+
+
+        #endregion
+    }
+}
---
+++ MealPlanner/UnitTests/Utilities/MockDbSet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace UnitTests.Utilities
+{
+    [ExcludeFromCodeCoverage]
+    public class MockDbSet<T> where T : class
+    {
+        public DbSet<T> Object { get; }
+
+        public MockDbSet(IEnumerable<T> list)
+        {
+            var queryableList = list.AsQueryable();
+            var mockDbSet = new Mock<DbSet<T>>();
+
+            mockDbSet.As<IAsyncEnumerable<T>>()
+                .Setup(m => m.GetEnumerator())
+                .Returns(new TestAsyncEnumerator<T>(queryableList.GetEnumerator()));
+
+            mockDbSet.As<IQueryable<T>>()
+                .Setup(m => m.Provider)
+                .Returns(new TestAsyncQueryProvider<T>(queryableList.Provider));
+
+            mockDbSet.As<IQueryable<T>>().Setup(x => x.Expression).Returns(queryableList.Expression);
+            mockDbSet.As<IQueryable<T>>().Setup(x => x.ElementType).Returns(queryableList.ElementType);
+            mockDbSet.As<IQueryable<T>>().Setup(x => x.GetEnumerator()).Returns(queryableList.GetEnumerator());
+            Object = mockDbSet.Object;
+        }
+    }
+}
