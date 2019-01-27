90c9244bab67634108e5f3b0f2110db7aa8ee339
rceleslie <ryan.celeslie@cbts.net>
2019-01-27 12:14:19 -0500

    Refactored repo tests to go off of BaseEntity instead of generic since the construction of my entities are off of BaseEntity, so it really doesn't matter.

--- MealPlanner/UnitTests/Infrastructure/Data/RepositoryTests.cs
+++ MealPlanner/UnitTests/Infrastructure/Data/RepositoryTests.cs
@@ -14,72 +14,90 @@ using Infrastructure.Data;
 
 namespace UnitTests.Infrastructure.Data
 {
-    public class RepositoryTests<T> where T : BaseEntity
+    public class RepositoryTests
     {
-        public MockDbSet<T> _dbSet { get; set; }
+        //TODO cleanup
+        //public MockDbSet<T> _dbSet { get; set; }
+        //public Mock<MealPlannerContext> _context { get; set; }
+        //public IRepository<T> _repo { get; set; }
+        //public IAsyncRepository<T> _asyncRepo { get; set; }
+
+        public Mock<DbSet<BaseEntity>> _dbSet { get; set; }
         public Mock<MealPlannerContext> _context { get; set; }
-        public IRepository<T> _repo { get; set; }
-        public IAsyncRepository<T> _asyncRepo { get; set; }
+        public IRepository<BaseEntity> _repo { get; set; }
+        public IAsyncRepository<BaseEntity> _asyncRepo { get; set; }
+
+        public RepositoryTests()
+        {
+            _dbSet = new Mock<DbSet<BaseEntity>>();
+            _context = new Mock<MealPlannerContext>(new DbContextOptions<MealPlannerContext>());
+            _repo = new EfRepository<BaseEntity>(_context.Object);
+            _asyncRepo = new EfRepository<BaseEntity>(_context.Object);
+        }
 
         #region Add
 
         // test for type
-        public void Test_Add_Entity_Returns_Type(T entity)
+        [Fact]
+        public void Test_Add_Entity_Returns_Type()
         {
             // Arrange
-            _context.Setup(c => c.Set<T>())
+            var mockEntity = new BaseEntity();
+
+            _context.Setup(c => c.Set<BaseEntity>())
                 .Returns(_dbSet.Object);
 
             // Act
-            var act = _repo.Add(entity);
+            var act = _repo.Add(mockEntity);
 
             // Assert
-            act.Should().BeOfType<T>();
+            act.Should().BeOfType<BaseEntity>();
         }
 
-        // test for type async
-        public async Task Test_AddAsync_Entity_Returns_Type(T entity)
-        {
-            // Arrange
-            _context.Setup(c => c.Set<T>())
-                .Returns(_dbSet.Object);
+        //TODO cleanup
+        //// test for type async
+        //public async Task Test_AddAsync_Entity_Returns_Type(T entity)
+        //{
+        //    // Arrange
+        //    _context.Setup(c => c.Set<T>())
+        //        .Returns(_dbSet.Object);
 
-            // Act
-            var act = await _asyncRepo.AddAsync(entity);
+        //    // Act
+        //    var act = await _asyncRepo.AddAsync(entity);
 
-            // Assert
-            act.Should().BeOfType<T>();
-        }
+        //    // Assert
+        //    act.Should().BeOfType<T>();
+        //}
 
-        // test for entity
-        public void Test_Add_Entity_Returns_Entity(T entity)
-        {
-            // Arrange
-            var mockEntity = entity;
-            _context.Setup(c => c.Set<T>())
-                .Returns(_dbSet.Object);
+        //// test for entity
+        //public void Test_Add_Entity_Returns_Entity(T entity)
+        //{
+        //    // Arrange
+        //    var mockEntity = entity;
+        //    _context.Setup(c => c.Set<T>())
+        //        .Returns(_dbSet.Object);
 
-            // Act
-            var act = _repo.Add(entity);
+        //    // Act
+        //    var act = _repo.Add(entity);
 
-            // Assert
-            act.Should().BeEquivalentTo(mockEntity);
-        }
+        //    // Assert
+        //    act.Should().BeEquivalentTo(mockEntity);
+        //}
 
-        // test for entity async
-        public async Task Test_AddAsync_Returns_Entity(T entity)
-        {
-            // Arrange
-            var mockEntity = entity;
-            _context.Setup(c => c.Set<T>())
-                .Returns(_dbSet.Object);
+        //// test for entity async
+        //public async Task Test_AddAsync_Returns_Entity(T entity)
+        //{
+        //    // Arrange
+        //    var mockEntity = entity;
+        //    _context.Setup(c => c.Set<T>())
+        //        .Returns(_dbSet.Object);
 
-            // Act
-            var act = await _asyncRepo.AddAsync(entity);
+        //    // Act
+        //    var act = await _asyncRepo.AddAsync(entity);
 
-            // Assert
-            act.Should().BeEquivalentTo(mockEntity);
-        }
+        //    // Assert
+        //    act.Should().BeEquivalentTo(mockEntity);
+        //}
         #endregion
 
         #region Count
@@ -90,7 +108,21 @@ namespace UnitTests.Infrastructure.Data
 
         #region Delete
 
+        //TODO cleanup
+        //// test for no error
+        //public void Test_Delete_Entity_No_Error()
+        //{
+        //    // Arrange
+        //    _context.Setup(c => c.Set<T>())
+        //        .Returns(_dbSet.Object);
 
+        //    // Act
+        //    _repo.Delete(entity);
+
+
+        //}
+
+        // test asyc for no error
 
         #endregion
 
