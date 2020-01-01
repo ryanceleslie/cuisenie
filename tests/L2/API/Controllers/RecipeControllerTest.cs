869060cb334523b597b19a30590b0469f8e0e5a3
Ryan Celeslie <ryan.celeslie@outlook.com>
2020-01-01 01:24:20 -0500

    Time for bed, don't want to lose my shit

---
+++ tests/L2/API/Controllers/RecipeControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Core.Entities.RecipeAggregate;
+
+namespace L2.API.Controllers
+{
+    [Collection("Sequential")]
+    public class RecipeControllerTest : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        public HttpClient Client { get; }
+
+        public RecipeControllerTest(CustomWebApplicationFactory<Startup> webFactory)
+        {
+            Client = webFactory.CreateClient();
+        }
+
+        #region Get
+
+        [Fact]
+        public async Task Get_Recipes()
+        {
+            // Arrange
+            var response = await Client.GetAsync("/recipe");
+            response.EnsureSuccessStatusCode();
+            var tempasdas = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<Recipe>(await response.Content.ReadAsStringAsync());
+
+            var temp = model;
+
+            // Act
+
+            // Assert 
+        }
+
+        #endregion
+
+        #region Get(int id)
+        #endregion
+
+        #region Put
+        #endregion
+
+        #region Post
+        #endregion
+
+        #region Delete
+        #endregion
+    }
+}
---
+++ tests/L2/CustomWebApplicationFactory.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace L2
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing");
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddEntityFrameworkInMemoryDatabase();
+
+                // Create a new service provider.
+                var provider = services
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                // Add a database context (ApplicationDbContext) using an in-memory 
+                // database for testing.
+                services.AddDbContext<MealPlannerContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(provider);
+                });
+
+                // Build the service provider.
+                var sp = services.BuildServiceProvider();
+
+                // Create a scope to obtain a reference to the database
+                // context (ApplicationDbContext).
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<MealPlannerContext>();
+                    var loggerFactory = scopedServices.GetRequiredService<ILoggerFactory>();
+
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    // Ensure the database is created.
+                    db.Database.EnsureCreated();
+
+                    //TODO eventually I do want to seed the DB with data
+                    //try
+                    //{
+                    //    // Seed the database with test data.
+                    //    CatalogContextSeed.SeedAsync(db, loggerFactory).Wait();
+
+                    //    // seed sample user data
+                    //    var userManager = scopedServices.GetRequiredService<UserManager<ApplicationUser>>();
+                    //    var roleManager = scopedServices.GetRequiredService<RoleManager<IdentityRole>>();
+                    //    AppIdentityDbContextSeed.SeedAsync(userManager, roleManager).Wait();
+                    //}
+                    //catch (Exception ex)
+                    //{
+                    //    logger.LogError(ex, $"An error occurred seeding the " +
+                    //        "database with test messages. Error: {ex.Message}");
+                    //}
+                }
+            });
+        }
+    }
+}
